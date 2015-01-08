64574648933ad588861ef484c5f01ac1fb432177
Musa.Zulu <zuluchs@gmail.com>
2015-01-08 09:39:02 +0200

    added new stringkata with new methods to perform different operations

---
+++ Thur08-01-2015/StringKataCalculator/StringKataCalculator/Calculator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringKataCalculator
+{
+    class Calculator
+    {
+        public int Add(string input)
+        {
+            if (IsNullOrEmpty(input))
+            {
+                return DefaultValue();
+            }
+            var delimiters = DefaultDelimiters();
+            if (HasCustormDelimiter(input))
+            {
+                input = GetNumbersAndDelimiters(input, ref delimiters);
+            }
+            var enumerable = GetNumbersAndSplitDelimiter(input, delimiters);
+           
+            return SumAll(enumerable);
+        }
+
+        private static string DefaultDelimiters()
+        {
+            return ",\n";
+        }
+
+        private static IEnumerable<string> GetNumbersAndSplitDelimiter(string input, string delimiters)
+        {
+            var numbers = Split(input, delimiters);
+            var enumerable = numbers as string[] ?? numbers.ToArray();
+            CheckNegative(enumerable);
+            return enumerable;
+        }
+
+        private static bool HasCustormDelimiter(string input)
+        {
+            return input.StartsWith("//");
+        }
+
+        private static string GetNumbersAndDelimiters(string input, ref string delimiters)
+        {
+            var index = input.IndexOf("\n");
+            delimiters += input.Substring(2, index - 2);
+            input = input.Substring(index + 1, input.Length - index - 1);
+            return input;
+        }
+
+
+        private static void CheckNegative(IEnumerable<string> numbers)
+        {
+            var negatives = numbers.Where(number => !IsEmpty(number) && IsNegative(number)).ToList();
+
+            if (negatives.Count > 0)
+            {
+                throw new ApplicationException("negatives are not allowed : " + string.Join(",", negatives));
+
+            }
+        }
+
+        private static bool IsNegative(string number)
+        {
+            return int.Parse(number) < 0;
+        }
+
+        private static bool IsEmpty(string number)
+        {
+            return number.Length == 0;
+        }
+
+        private static int SumAll(IEnumerable<string> numbers)
+        {
+            return numbers.Where(number => !IsEmpty(number) && InRange(number)).Sum(number => int.Parse(number));
+        }
+
+        private static bool InRange(string number)
+        {
+            return int.Parse(number) <= 1000;
+        }
+
+        private static IEnumerable<string> Split(string input, string delimiters)
+        {
+            return input.Split(delimiters.ToCharArray(), StringSplitOptions.None);
+        }
+
+
+        private static int DefaultValue()
+        {
+            return 0;
+        }
+
+        private static bool IsNullOrEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
---
+++ Thur08-01-2015/StringKataCalculator/StringKataCalculator/TestCalculator.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringKataCalculator
+{
+    [TestFixture]
+    public class TestCalcator
+    {
+        [Test]
+        public void Given_EmptyStringInputShould_ReturnZero()
+        {
+            const string input = "";
+            const int expected = 0;
+            var calculator = new Calculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithSingleNumberShould_ReturnNumber()
+        {
+            const string input = "1";
+            const int expected = 1;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithLargeSingleNumberShould_ReturnNumber()
+        {
+            const string input = "185";
+            const int expected = 185;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithTwoNumbersShould_ReturnSum()
+        {
+            const string input = "1,8";
+            const int expected = 9;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithManyNumbersShould_ReturnSum()
+        {
+            const string input = "1,2,3,4,5,6,7,8";
+            const int expected = 36;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithNewLineBetweenNumbersShould_ReturnSum()
+        {
+            const string input = "1\n2,3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithDelimiterBetweenNumbersShould_ReturnSum()
+        {
+            const string input = "//;\n1;2";
+            const int expected = 3;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+
+        [Test]
+        public void Given_StringInputWithNegativeNumberShould_ThrowException()
+        {
+            const string input = "-1,2";
+            const string expected = "negatives are not allowed : -1";
+            var calculator = CreateCalculator();
+            var results = Assert.Throws<ApplicationException>(() => calculator.Add(input));
+
+            Assert.AreEqual(expected, results.Message);
+        }
+
+
+        [Test]
+        public void Given_StringInputWithNegativeNumbersShould_ThrowException()
+        {
+            const string input = "-1,2,-3,-4";
+            const string expected = "negatives are not allowed : -1,-3,-4";
+            var calculator = CreateCalculator();
+            var results = Assert.Throws<ApplicationException>(() => calculator.Add(input));
+
+            Assert.AreEqual(expected, results.Message);
+        }
+
+
+        [Test]
+        public void Given_StringInputWithNumberGreaterThanThousandShould_IgnoreNumberReturnSum()
+        {
+
+            const string input = "1001,3";
+            const int expected = 3;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+
+        [Test]
+        public void Given_StringInputWithNumberNotGreaterThanThousandShould_ReturnSum()
+        {
+
+            const string input = "1000,3";
+            const int expected = 1003;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithDelimitersInBetweenNumbersShould_ReturnSum()
+        {
+
+            const string input = "//[***]\n1***2***3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Given_StringInputWithMultipleDelimitersInBetweenNumbersShould_ReturnSum()
+        {
+
+            const string input = "//[*][%]\n1*2%3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+
+        [Test]
+        public void Given_StringInputWithMultipleDelimitersOfAnyLengthInBetweenNumbersShould_ReturnSum()
+        {
+
+            const string input = "//[*][%][@][#]\n1*2%3@4#5";
+            const int expected = 15;
+            var calculator = CreateCalculator();
+            var results = calculator.Add(input);
+
+            Assert.AreEqual(expected, results);
+        }
+
+        private static Calculator CreateCalculator()
+        {
+            return new Calculator();
+        }
+    }
+}
