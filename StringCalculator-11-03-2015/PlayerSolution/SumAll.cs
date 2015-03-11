c71e12efe1f525f0aa87f3f7235a05b39a8534ee
Musa.Zulu <zuluchs@gmail.com>
2015-03-11 09:00:26 +0200

    added new string kata

---
+++ StringCalculator-11-03-2015/PlayerSolution/SumAll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayerStringKata
+{
+    public class SumAll
+    {
+        public static int SumAllNumbers(IEnumerable<string> numbers)
+        {
+            return numbers.Select(StringCalculator.NumberParser()).Where(StringCalculator.IsInRange()).Sum();
+        }
+    }
+}
