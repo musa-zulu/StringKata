93a3abaf34159c145e09133263ff3101b09f6a43
Musa.Zulu <zuluchs@gmail.com>
2015-05-07 08:20:52 +0200

    added new string kata

---
+++ StringCalculator_07_05_2015/PlayerSolution/TestStringCalculator.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using Engine;
+using Katarai.StringCalculator.Interfaces;
+using NUnit.Framework;
+
+namespace PlayerStringKata
+{
+    public class TestStringCalculator : ITestPack<IStringCalculator>
+    {
+        public Func<IStringCalculator> CreateSUT { get; set; }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            CreateSUT = () => new StringCalculator();
+        }
+
+        private IStringCalculator CreateCalculator()
+        {
+            return CreateSUT();
+        }
+
+        /// <summary>
+        /// This is a sample test that shows how the StringCalculator 
+        /// should be created in future tests. 
+        /// </summary>
+        [Test]
+        public void Constructor()
+        {
+            //---------------Arrange-------------------
+
+            //---------------Act----------------------
+            var calculator = CreateCalculator();
+            //---------------Assert-----------------------
+            Assert.IsNotNull(calculator);
+        }
+
+        [Test]
+        public void Add_GivenEmptyInputString_ShouldReturnZero()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "";
+            const int expected = 0;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenSingleNumberInputString_ShouldReturnSingleNumber()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1";
+            const int expected = 1;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenLargeSingleNumberInputString_ShouldReturnSingleNumber()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "120";
+            const int expected = 120;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenTwoNumbersInputString_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1,2";
+            const int expected = 3;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenManyNumbersInputString_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1,2,3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithNewLine_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1\n2,3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithCustomDelimiter_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "//;\n1;2";
+            const int expected = 3;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithNegativeNumber_ShouldThrowException()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "-1";
+            var expected = new List<int> { -1 };
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = Assert.Throws<NegativesNotAllowedException>(() => calculator.Add(input));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual.NegativeNumbers);
+        }
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithNegativeNumbers_ShouldThrowException()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "-1,-3";
+            var expected = new List<int> { -1, -3 };
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = Assert.Throws<NegativesNotAllowedException>(() => calculator.Add(input));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual.NegativeNumbers);
+        }
+
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithNumberGreaterThanThousa_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1001,3";
+            const int expected = 3;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        } [Test]
+        public void Add_GivenManyNumbersInputStringWithNumberNotGreaterThanThousa_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "1000,3";
+            const int expected = 1003;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenManyNumbersInputStringWithCustomDelimitersOfAnyLength_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "//[***]\n1***2***3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        } [Test]
+        public void Add_GivenManyNumbersInputStringWithDifferentCustomDelimitersOfAnyLength_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            const string input = "//[$$*][%****]\n1$$*2%****3";
+            const int expected = 6;
+            var calculator = CreateCalculator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var actual = calculator.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
